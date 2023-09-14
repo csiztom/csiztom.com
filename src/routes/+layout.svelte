854c4bfffca7aa5cd2ba02bd112f5e8291ca5292
csiztom <tom.csiz@gmail.com>
2023-09-15 01:02:19 +0200

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import '../app.css'
 	import 'css.gg/icons/icons.css'
+	import { dev } from '$app/environment'
+	import { inject } from '@vercel/analytics'
+	inject({ mode: dev ? 'development' : 'production' })
 </script>
 
 <div class="app">
