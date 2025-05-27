84c38d71bdb071358efbe1283fe737ed6679e90c
csiztom <tom.csiz@gmail.com>
2025-05-28 00:20:28 +0200

    fix mobile view

--- src/lib/components/Introduction.svelte
+++ src/lib/components/Introduction.svelte
@@ -2,15 +2,15 @@
 	import { t } from '$lib/utils/i18n'
 </script>
 
-<div class="grid grid-cols-2 h-full">
+<div class="grid grid-cols-1 lg:grid-cols-2 h-full p-10 gap-10">
 	<div class="flex justify-center items-center shrink-0">
 		<div class="h-fit">
-			<h1 class="text-9xl">
+			<h1 class="text-8xl lg:text-9xl">
 				{$t('intro.hello')}<br />{$t('intro.iam', { name: 'Tamás' })}
 			</h1>
 		</div>
 	</div>
-	<div class="flex justify-center items-center p-24 shrink-0">
-		<p class="text-4xl">{$t('intro.desc')}</p>
+	<div class="flex justify-center items-center shrink-0">
+		<p class="text-3xl lg:text-4xl">{$t('intro.desc')}</p>
 	</div>
 </div>
--- src/lib/components/LocaleSelector.svelte
+++ src/lib/components/LocaleSelector.svelte
@@ -2,18 +2,17 @@
 	import { locale, locales } from '$lib/utils/i18n'
 </script>
 
-<div class="absolute top-0 right-5 group">
-	<div
-		class="p-2 glass-effect rounded-b-2xl flex flex-col justify-center items-center gap-2"
-	>
-		<button class="btn btn-square mb-1" disabled>{$locale}</button>
-		{#each locales as l (l)}
-			{#if l !== $locale}
-				<button
-					class="btn btn-square group-hover:block hidden"
-					on:click={() => ($locale = l)}>{l}</button
-				>
-			{/if}
-		{/each}
+<div class="absolute top-0 right-5">
+	<div class="p-2 rounded-b-2xl gap-2 dropdown dropdown-end">
+		<div role="button" tabindex="0" class="btn btn-square mb-1">{$locale}</div>
+		<ul class="dropdown-content menu bg-base-100 rounded-box z-1 p-2 shadow-sm">
+			{#each locales as l (l)}
+				{#if l !== $locale}
+					<li>
+						<button tabindex="0" on:click={() => ($locale = l)}>{l}</button>
+					</li>
+				{/if}
+			{/each}
+		</ul>
 	</div>
 </div>
